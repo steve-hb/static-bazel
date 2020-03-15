221ab17541810c75956729a9d6610ce4796b0179
Steve Hb <stve.hb@gmail.com>
2020-03-15 04:04:41 +0100

    Added test environment
    Signed-off-by: Steve Hb <stve.hb@gmail.com>

---
+++ test/java/helloWorld/src/main/java/com/example/bazel/test/java/hello/world/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.bazel.test.java.hello.world;
+
+public class HelloWorld {
+
+	public static void main(String... args) {
+		Logger.log("Hello World!");
+	}
+
+}
---
+++ test/java/logger/src/main/java/com/example/bazel/test/java/hello/world/Logger.java
@@ -0,0 +1,9 @@
+package com.example.bazel.test.java.hello.world;
+
+public class Logger {
+
+	public static void log(String message) {
+		System.out.println("Message: " + message);
+	}
+
+}
